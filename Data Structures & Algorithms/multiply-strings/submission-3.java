75c1dbceb4266ef30aacb704d43c9f0745a97590
Tomris Teymurlu <tomristt9@gmail.com>
2026-04-18 14:57:31 +0400

    Add: multiply-strings - submission-3

---
+++ Data Structures & Algorithms/multiply-strings/submission-3.java
@@ -0,0 +1,28 @@
+
+class Solution {
+    public String multiply(String num1, String num2) {
+    
+        int m = num1.length();
+        int n = num2.length();
+        int[] res = new int[m+n];
+        for(int i=n-1;i >=0; i--){
+            for(int j=m-1; j>=0; j--){
+                int mul = (num2.charAt(i)-'0')*(num1.charAt(j)-'0');
+            int sum = mul + res[i + j + 1];
+
+                res[i + j + 1] = sum % 10;
+                res[i + j] += sum / 10;
+            }
+        }
+
+
+        StringBuilder sb = new StringBuilder();
+        for (int x : res) {
+            if (sb.length() != 0 || x != 0) {
+                sb.append(x);
+            }
+        }
+
+        return sb.length() == 0 ? "0" : sb.toString();
+    }
+}
